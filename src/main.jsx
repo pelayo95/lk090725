49eadca72f1f5f4875fa21e9b07092776428d217
pelayo95 <pelayover@gmail.com>
2025-07-09 17:01:01 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppWrapper from './AppWrapper.js'
+import AppWrapper from './AppWrapper.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
