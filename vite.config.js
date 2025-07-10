21efefa01eb8225480114daaf65130a5477bb7e6
pelayo95 <pelayover@gmail.com>
2025-07-10 08:15:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <-- ESTA ES LA LÍNEA CRUCIAL QUE SE AÑADIÓ
 })
