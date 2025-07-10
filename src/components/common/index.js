71857c01f2fdb84adf46ed27aecd045e95fcae86
pelayo95 <pelayover@gmail.com>
2025-07-09 20:10:22 -0400

    Update index.js

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,11 +1,12 @@
 // src/components/common/index.js
 // Este archivo exporta todos los componentes comunes para facilitar las importaciones.
 
-export * from './Button';
-export * from './Card';
-export * from './Input';
-export * from './Select';
-export * from './TextArea';
-export * from './Modal';
-export * from './ConfirmationModal';
-export * from './Tooltip';
+export * from './Button.jsx';
+export * from './Card.jsx';
+export * from './Input.jsx';
+export * from './Select.jsx';
+export * from './TextArea.jsx';
+export * from './Modal.jsx';
+export * from './ConfirmationModal.jsx';
+export * from './Tooltip.jsx';
+export * from './AddItemModal.jsx';
